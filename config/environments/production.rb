ec2c51bf193fb6897258f9e2332c0d09f4e3fbb0
Sidharth Baveja <sidharthbav@gmail.com>
2023-03-05 09:19:22 -0600

    adding missing comma in the production.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,7 +100,7 @@ Rails.application.configure do
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :user_name            => ENV["PUBLIC_HEALTH_EMAIL"],
-    :password             => ENV["PUBLIC_HEALTH_EMAIL_PASSWORD"]
+    :password             => ENV["PUBLIC_HEALTH_EMAIL_PASSWORD"],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
