d880e5623a33a47c2aba62565a5b6c207ccce610
Arefe <pouyan.forghani@gmail.com>
2023-03-31 12:43:31 -0500

    correcting score calculation method in the model

--- app/models/response.rb
+++ app/models/response.rb
@@ -5,8 +5,7 @@ class Response < ApplicationRecord
   def calculate_response_score
     question = Question.find_by(survey_id: survey_id, question_number: question_number)
     option_points_list = question.option_points_list
-    option_points_hash = option_points_list.split(',').map { |str| str.split(':') }.to_h
-    self.response_score = option_points_hash[response]
+    response_score = option_points_list[response_option_number]
     save
   end
 end
