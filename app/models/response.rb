062fe647d4d00a451ff3ad151eac7b2ceaedb450
Sidharth Baveja <sidharthbav@gmail.com>
2023-04-13 14:00:29 -0500

    fixing the seeding issue

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,7 +1,6 @@
 class Response < ApplicationRecord
   belongs_to :survey
   belongs_to :user
-  has_and_belongs_to_many :category
 
   def calculate_response_score
     question = Question.find_by(survey_id: survey_id, question_number: question_number)
--- app/models/survey_result.rb
+++ app/models/survey_result.rb
@@ -1,5 +1,3 @@
 class SurveyResult < ApplicationRecord
   belongs_to :survey
-  attr_accessor :categories_id
-  has_and_belongs_to_many :categories
 end
