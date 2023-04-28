4bc761bccaa59440c9aa5f81b19171a65c01e3f8
Sidharth Baveja <sidharthbav@gmail.com>
2023-04-28 10:53:20 -0500

    commenting out a function in registration controller and updating the code coverage report

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -51,9 +51,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # Overwriting the sign_up redirect path method
-  def after_sign_up_path_for(resource_or_scope)
-    new_user_session_path
-  end
+  # def after_sign_up_path_for(resource_or_scope)
+  #   new_user_session_path
+  # end
 
     # Overwriting the sign_up redirect path method
     def after_inactive_sign_up_path_for(resource_or_scope)
